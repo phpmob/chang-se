a39f24b89b6facdc7b388975aa2c22f675a3e25a
liverbool <nukboon@gmail.com>
2018-10-07 01:02:18 +0700

    improve child menu

--- private/modern/app.js
+++ private/modern/app.js
@@ -4,7 +4,7 @@ window.$ = window.jQuery = require('jquery');
 require('jquery-confirm');
 require('bootstrap');
 
-require('chang/js/data-confirmation');
+require('chang/js/data-confirm');
 require('chang/js/data-ajax-form');
 require('chang/js/form-collection');
 require('chang/js/form-validator-html5');
